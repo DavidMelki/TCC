b7ff74050fddf73ee717e4e5e69805fe306bd045
Julia Akemi <schwtsz13@gmail.com>
2026-08-13 19:06:23 -0300

    banco criado

--- eicamargo/php/sugestao.php
+++ eicamargo/php/sugestao.php
@@ -15,6 +15,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // =========================================================================
     // FUTURO BANCO DE DADOS:
     // Aqui você fará o INSERT apenas da descrição (e usuário/data se houver).
+    require_once "conexao.php";
+
+try {
+    $sql = "INSERT INTO sugestoes (descricao) VALUES (:descricao)";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":descricao", $descricao);
+    $stmt->execute();
+
+    echo json_encode([
+        'status' => 'success',
+        'message' => 'Sugestão cadastrada com sucesso!'
+    ]);
+
+} catch (PDOException $e) {
+
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Erro ao cadastrar sugestão.'
+    ]);
+}
     // =========================================================================
 
     echo json_encode([
